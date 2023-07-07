3ee022c90a1d5f6d41c20534068f422d11d2a249
Ainur <ayazbay.ainur@mail.ru>
2023-07-08 03:31:00 +0600

    3rd commit

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,7 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
-        if(true) System.out.println(true);
+        if(true){
+            System.out.println(true);
+        }
         else{}
     }
 }
